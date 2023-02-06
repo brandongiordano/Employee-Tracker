cf7ef72bb23add2c6336bbd48be794531723bbe6
brandongiordano <bgiordano9917@aol.com>
2023-02-06 13:14:09 -0500

    made add functions

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ const menu = [
   }
 ];
 
+// Init function prompts user and allows use of the database
 function init() {
     inquirer
         .prompt(menu)
@@ -95,5 +96,117 @@ function viewDepts() {
         init();
     })
 }
+
+function viewRoles() {
+    db.query("SELECT role.id AS Dept_ID, role.title AS Title FROM role",
+    function(err, res) {
+        if (err) throw err
+        console.log("Roles");
+        console.table(res);
+        init();
+    })
+}
+
+function viewEmployees() {
+    db.query("SELECT employee.id AS Employee.ID, SELECT first_name AS First_Name, SELECT last_name AS Last_Name, SELECT role_id AS Role_ID, SELECT manager_id AS Manager_ID FROM employee",
+    function(err, res) {
+        if (err) throw err
+        console.log("Employees");
+        console.table(res);
+        init();
+    })
+}
+
+// Array for adding Department to database
+const deptArr = [];
+function selectDept() {
+  db.query("SELECT * FROM department", function(err, res) {
+    if (err) throw err
+    for (let i = 0; i < res.length; i++) {
+      deptArr.push(res[i].name);
+    }
+})
+return deptArr;
+}
+
+// Add Dept
+function addDept() { 
+    inquirer.prompt([
+        {
+          name: "name",
+          type: "input",
+          message: "What Department would you like to add? "
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "What is the new Department ID number? "
+          }
+
+    ]).then(function(answers) {
+        db.query("INSERT INTO department SET ? ",
+            {
+              name: answers.name,
+              id: answers.id
+            },
+            function(err) {
+                if (err) throw err
+                console.table(res);
+                init();
+            }
+        )
+    })
+};
+
+// Array for adding Role
+const roleArr = [];                                            
+function selectRole() {
+  db.query("SELECT * FROM role", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      roleArr.push(res[i].title);
+    }
+  })
+  return roleArr;
+}
+
+// Add Department
+function addRole() { 
+    db.query("SELECT role.title AS Title, role.salary AS Salary FROM role LEFT JOIN department.name AS Department FROM department;",   function(err, res) {
+      inquirer.prompt([
+          {
+            name: "title",
+            type: "input",
+            message: "What is name of the new role?"
+          },
+          {
+            name: "salary",
+            type: "input",
+            message: "What is the salary of the new role?"
+          } ,
+          {
+            name: "department",
+            type: "rawlist",
+            message: "Under which department does this new role fall?",
+            choices: selectDept()
+          }
+      ]).then(function(answers) {
+          var deptId = selectDept().indexOf(answers.choice) + 1
+          db.query(
+              "INSERT INTO role SET ?",
+              {
+                title: answers.title,
+                salary: answers.salary,
+                departmentID: deptId
+              },
+              function(err) {
+                  if (err) throw err
+                  console.table(answers);
+                  init();
+              }
+          )     
+      });
+    });
+};
 
 init();
